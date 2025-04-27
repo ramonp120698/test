9c9823198be7302c23dda38a728e0a30c4d39b29
ramonp120698 <ramonp.128@gmail.com>
2025-04-27 13:26:36 -0700

    Update pagina_C.py

--- pages/pagina_C.py
+++ pages/pagina_C.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import utilidades as util
 util.generarMenu()
 st.header('Página C')
