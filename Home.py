d3b8579f038e7933557032488bb462c151f42f67
ramonp120698 <ramonp.128@gmail.com>
2025-04-27 13:44:07 -0700

    Delete Home.py

--- Home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Título principal
-st.title("Bienvenido a mi Aplicación 🚀")
-
-st.write("""
-Este es el Home de tu aplicación.
-Utiliza el menú lateral para navegar entre las páginas.
-""")
