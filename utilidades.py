ef5c37ebec755ddda6cdabaf62b95e6c16a61f48
ramonp120698 <ramonp.128@gmail.com>
2025-04-27 13:07:15 -0700

    Delete utilidades.py

--- utilidades.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def generarMenu():
-    with st.sidebar:
-        st.page_link('appPrincipal.py',label="Inicio", icon="🏚️")
-        st.page_link('pages/1_pagina_A.py',label="Tablero A", icon="📄")
-        st.page_link('pages/2_pagina_B.py',label="Tablero B", icon="📄")
