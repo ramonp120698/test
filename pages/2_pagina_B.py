95e7eabab2b4f84b287fbb4610bbce005344e193
ramonp120698 <ramonp.128@gmail.com>
2025-04-27 13:16:19 -0700

    Update 2_pagina_B.py

--- pages/2_pagina_B.py
+++ pages/2_pagina_B.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import utilidades as util
 util.generarMenu()
 st.header('Página B')
