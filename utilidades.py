b94cd6ec794261f81f7ee54d7ec9e5f2a2398147
ramonp120698 <ramonp.128@gmail.com>
2025-04-27 18:17:04 -0700

    Update utilidades.py

--- utilidades.py
+++ utilidades.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def generarMenu():
     with st.sidebar:
-        st.page_link('appPrincipal.py',label="Home", icon="🏠")
-        st.page_link('pages/1_pagina_A.py',label="TAKA", icon="📄")
-        st.page_link('pages/2_pagina_B.py',label="BYGA", icon="📄")
-        st.page_link('pages/pagina_C.py',label="A-Champs", icon="📄")
+        st.page_link('appPrincipal.py',label="Home", icon="⚽")
+        st.page_link('pages/1_pagina_A.py',label="TAKA", icon="🎦")
+        st.page_link('pages/2_pagina_B.py',label="BYGA", icon="📊")
+        st.page_link('pages/pagina_C.py',label="A-Champs", icon="🏆")
